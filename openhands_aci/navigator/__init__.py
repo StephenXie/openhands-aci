c4ae06a3af6bafdad907ef8d3fb1c2cd3157255b
Hoang Tran <descience.thh10@gmail.com>
2024-12-23 16:06:20 +0700

    add tool & param desc for nav

--- openhands_aci/navigator/__init__.py
+++ openhands_aci/navigator/__init__.py
@@ -6,6 +6,19 @@ from .navigator import Command, SymbolNavigator
 _GLOBAL_NAVIGATOR = SymbolNavigator()
 
 
+TOOL_DESCRIPTION = """Custom navigation tool for navigating to symbols in a codebase
+* If there are multiple symbols with the same name, the tool will print all of them
+* The `jump_to_definition` command will print the FULL definition of the symbol, along with the absolute path to the file
+* The `find_references` command will print only the file content at the line where the symbol is referenced, along with the absolute path to the file
+* It is more preferable to use this tool for user-defined symbols. For built-in symbols, consider using other tools like `grep`
+"""
+
+PARAMS_DESCRIPTION = {
+    'command': 'The command to run. Allowed options are: `jump_to_definition`, `find_references`.',
+    'symbol_name': 'The symbol name to navigate to.',
+}
+
+
 def symbol_navigator(
     command: Command,
     symbol_name: str,
